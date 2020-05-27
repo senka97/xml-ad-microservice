58159366cfc9f934918646852886beda24063d79
Senka <senka.soic@uns.ac.rs>
2020-05-27 13:18:47 +0200

    Project created.

---
+++ src/main/java/com/team19/admicroservice/AdmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.team19.admicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team19/admicroservice/AdmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team19.admicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

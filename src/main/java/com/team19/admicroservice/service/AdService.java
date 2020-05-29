34291c7c4da0180590b141b30e4ad9ebcb8f80eb
Aleksandra Randjelovic <aleksandra.randjelovic19@gmail.com>
2020-05-29 16:47:37 +0200

    Activating and blockig client is done

--- src/main/java/com/team19/admicroservice/service/AdService.java
+++ src/main/java/com/team19/admicroservice/service/AdService.java
@@ -3,6 +3,7 @@ package com.team19.admicroservice.service;
 import com.team19.admicroservice.dto.AdDTO;
 import com.team19.admicroservice.dto.CartItemDTO;
 import com.team19.admicroservice.dto.AdDTOSimple;
+import com.team19.admicroservice.model.Ad;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,4 +17,6 @@ public interface AdService {
     Long getAdOwner(Long id);
     List<CartItemDTO> fillCartItems(List<CartItemDTO> cartItemDTOs);
     AdDTOSimple getAdSimple(Long id);
+    boolean hideAdsForBlockedClient(Long id);
+    boolean showAdsForActiveClient(Long id);
 }

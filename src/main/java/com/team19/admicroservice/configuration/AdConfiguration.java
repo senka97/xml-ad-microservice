c1df5a9ff4822a723862d33634aac28da7647705
Senka <senka.soic@uns.ac.rs>
2020-06-03 22:11:42 +0200

    Added some permitted paths for unauthorised users.

--- src/main/java/com/team19/admicroservice/configuration/AdConfiguration.java
+++ src/main/java/com/team19/admicroservice/configuration/AdConfiguration.java
@@ -47,7 +47,7 @@ public class AdConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("/h2-console/**", "/api/ad/**","/api/search/**","/api/ads/**","/api/canPost/**")
+                .antMatchers("/h2-console/**", "/api/ad/**","/api/search/**","/api/ads/**","/api/canPost/**","/api/getAd/**")
                 .permitAll()
                 .anyRequest().authenticated();
 

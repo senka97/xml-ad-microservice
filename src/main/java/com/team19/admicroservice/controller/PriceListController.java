79b22b7660315d501521ecd6e79e569edd5adf81
Senka <senka.soic@uns.ac.rs>
2020-06-15 19:43:08 +0200

    Retrieving price list dto done.

--- src/main/java/com/team19/admicroservice/controller/PriceListController.java
+++ src/main/java/com/team19/admicroservice/controller/PriceListController.java
@@ -5,6 +5,7 @@ import com.team19.admicroservice.service.impl.PriceListServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,14 +19,23 @@ public class PriceListController {
     private PriceListServiceImpl priceListService;
 
     @GetMapping(value = "/priceList/owner")
+    @PreAuthorize("hasAuthority('priceList_read')")
     ResponseEntity<?> getUsersPriceLists(){
         return new ResponseEntity<>(priceListService.getPriceListsFromUser(), HttpStatus.OK);
 
     }
 
     @PostMapping(value = "/priceList/ads")
+    @PreAuthorize("hasAuthority('priceList_read')")
     ResponseEntity<?> findPriceLists(@RequestBody List<PriceListAdDTO> priceListAdDTOs){
 
           return new ResponseEntity(this.priceListService.findPriceLists(priceListAdDTOs), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/priceList/ad/{id}")
+    @PreAuthorize("hasAuthority('priceList_read')")
+    ResponseEntity<?> getPriceListForAd(@PathVariable("id") Long adId){
+
+        return new ResponseEntity<>(priceListService.getPriceListForAd(adId), HttpStatus.OK);
     }
 }
